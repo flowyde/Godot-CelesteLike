dfac6d6494a8a501c899ae7ba1b8de4c823eddc3
flowyde <69410739+flowyde@users.noreply.github.com>
2025-08-01 21:48:22 -0300

    changed the player movement code
    still kinda buggy :c

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,27 @@
 extends CharacterBody2D
 
-var speed : int = 20
+const max_speed = 200
+const acceleration = 8000
 
 func _ready() -> void:
-	
 	pass
 	
 func _process(delta: float) -> void:
 	pass
 	
-func _physics_process(delta: float) -> void:
-	var hMove: int = Input.get_axis("move_right","move_left")
-	velocity.x += hMove * speed
+func get_input():
+	var hMove = Input.get_axis("move_left","move_right")
+	if hMove != 0:
+		velocity.x += hMove * acceleration
+		velocity.x = clamp(velocity.x, -max_speed, max_speed)
+	else:
+		velocity.x = 0
+		
 	velocity.y += get_gravity().y
+	
+	print(velocity.x)
+
+func _physics_process(delta: float) -> void:
+	get_input()
 	move_and_slide()
 	pass
